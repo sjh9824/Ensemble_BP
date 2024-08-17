609eb365cc504f3cdad3320781abd276699d2628
JaeHyuk Son <73771922+sjh9824@users.noreply.github.com>
2024-08-17 19:27:22 +0900

    Update vid_to_npy.py

--- Ensemble_Pred_BP/Data/vid_to_npy.py
+++ Ensemble_Pred_BP/Data/vid_to_npy.py
@@ -1,29 +1,14 @@
 import os
-import sys
-import cv2
-import numpy as np
+import glob
+from collections import defaultdict
 import json
 import random
+import cv2
+import numpy as np
 from tqdm import tqdm
 from retinaface import RetinaFace
 from mtcnn import MTCNN
 import torch
-import logging
-
-# Logging 설정
-logging.basicConfig(filename='warnings.log', level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
-
-class VideoCaptureSuppressor:
-    def __enter__(self):
-        self.stdout = sys.stdout
-        self.stderr = sys.stderr
-        sys.stdout = open(os.devnull, 'w')
-        sys.stderr = open(os.devnull, 'w')
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout = self.stdout
-        sys.stderr = self.stderr
 
 class FaceDetector:
     def __init__(self):
@@ -32,18 +17,19 @@ class FaceDetector:
     def detect_and_resize_face(self, image, frame_count, target_size=(128, 128), backend='HC', use_larger_box=False, larger_box_coef=2.0):
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
+        global device_printed
         
         face_box_coor = None
         
         current_dir = os.path.dirname(os.path.abspath(__file__))
         hc_path = os.path.join(current_dir, 'haarcascade_frontalface_default.xml')
         
+        # HC 백엔드는 기존처럼 처리
         if backend == "HC":
             detector = cv2.CascadeClassifier(hc_path)
             face_zone = detector.detectMultiScale(image_rgb)
 
             if len(face_zone) < 1:
-                logging.warning("No Face Detected - Using last detected face position")
                 return None
             elif len(face_zone) >= 2:
                 max_width_index = np.argmax(face_zone[:, 2])
@@ -51,6 +37,7 @@ class FaceDetector:
             else:
                 face_box_coor = face_zone[0]
 
+        # RF와 MT 백엔드는 frame_count에 따라 처리
         elif backend == "RF" or backend == "MT":
             if frame_count == 0 or frame_count % 160 == 0 or self.last_face_box_coor is None:
                 if backend == "RF":
@@ -72,14 +59,13 @@ class FaceDetector:
                         new_y = center_y - (square_size // 2)
                         face_box_coor = [new_x, new_y, square_size, square_size]
                     else:
-                        logging.warning("No Face Detected - Using last detected face position")
+                        print("ERROR: No Face Detected - Using last detected face position")
                         face_box_coor = self.last_face_box_coor
 
                 elif backend == "MT":
                     detector = MTCNN(keep_all=True, device=device)
                     results = detector.detect_faces(image_rgb)
                     if len(results) < 1:
-                        logging.warning("No Face Detected - Using last detected face position")
                         return None
                     elif len(results) >= 2:
                         max_width_index = np.argmax([r['box'][2] for r in results])
@@ -87,24 +73,25 @@ class FaceDetector:
                     else:
                         face_box_coor = results[0]['box']
 
+                # 얼굴 위치를 저장
                 self.last_face_box_coor = face_box_coor
             else:
+                # 이전에 검출된 얼굴 위치 사용
                 face_box_coor = self.last_face_box_coor
 
+        # 더 큰 박스를 사용할 경우
         if use_larger_box:
             face_box_coor[0] = max(0, face_box_coor[0] - (larger_box_coef - 1.0) / 2 * face_box_coor[2])
             face_box_coor[1] = max(0, face_box_coor[1] - (larger_box_coef - 1.0) / 2 * face_box_coor[3])
             face_box_coor[2] = int(larger_box_coef * face_box_coor[2])
             face_box_coor[3] = int(larger_box_coef * face_box_coor[3])
             
+        # 얼굴 크롭 및 리사이즈
         x, y, w, h = face_box_coor
         face_crop = image[int(y):int(y+h), int(x):int(x+w)]
         resized_face = cv2.resize(face_crop, target_size, interpolation=cv2.INTER_AREA)
 
         return resized_face
-
-def handle_video_decoding_error(error):
-    logging.warning(f"h264 decoding error: {error}")
 
 class convert_vid2npy():
     def __init__(self, config, path):
@@ -115,6 +102,7 @@ class convert_vid2npy():
         self.backend = config['PREPROCESS']['BACKEND']
         self.chunk_size = config['PREPROCESS']['CHUNK_LENGTH']
         
+        
     def vid2npy(self):
         with open(self.vid_path, 'r') as f:
             data = json.load(f)
@@ -123,12 +111,14 @@ class convert_vid2npy():
         self.save_to_json(processed_data)
         
     def process_all_subjects(self, data, output_dir):
+    # 완료된 subject 목록을 로드 또는 초기화        
         with tqdm(total=len(data.keys()), desc="Processing Subjects", unit="subject") as pbar_subjects:
             processed_data = {}
             current_dir = os.path.dirname(os.path.abspath(__file__))
-            output_dir = os.path.join(current_dir, 'Process')
+            output_dir = config_file_path = os.path.join(current_dir, 'Process')
             for subject in data.keys():
                 
+                # 기존 코드로 subject 처리
                 details = data[subject]
                 video_paths = details['video']
                 BP = details['BP']
@@ -136,9 +126,11 @@ class convert_vid2npy():
                 yuv_files = []
                 npy_index = 0
 
+                
+                #HC, RF, MT
                 for video_path in video_paths:
                     subject_output_dir = os.path.join(output_dir, subject)
-                    rgb_npy_files, yuv_npy_files, npy_index = self.process_video(video_path, subject_output_dir, target_size=(self.H,self.W), backend=self.backend, chunk_size=self.chunk_size, start_idx=npy_index, subject_name=subject)
+                    rgb_npy_files, yuv_npy_files, npy_index = self.process_video(video_path, subject_output_dir, target_size=(self.H,self.W), backend=self.backend, chunk_size=self.chunk_size,start_idx=npy_index,subject_name=subject)
                     rgb_files.extend(rgb_npy_files)
                     yuv_files.extend(yuv_npy_files)
 
@@ -175,57 +167,59 @@ class convert_vid2npy():
         if not os.path.exists(yuv_output_dir):
             os.makedirs(yuv_output_dir)
 
-        with VideoCaptureSuppressor():  # Suppress the warnings
-            cap = cv2.VideoCapture(video_path)
-            if not cap.isOpened():
-                handle_video_decoding_error(f"Failed to open video file: {video_path}")
-                return [], [], start_idx
-            total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-            frame_count = 0
-            face_frames = []
-            rgb_files = []
-            yuv_files = []
-            npy_index = start_idx
-            
-            total_num = total_frames - (total_frames % 160)
+        cap = cv2.VideoCapture(video_path)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_count = 0
+        face_frames = []
+        rgb_files = []
+        yuv_files = []
+        npy_index = start_idx
+        
+        total_num = total_frames - (total_frames % 160)
 
-            with tqdm(total=total_num, desc=f"Processing {subject_name}", unit="frame", leave=False) as pbar_frames:
-                while True:
-                    ret, frame = cap.read()
-                    if not ret:
-                        break
+        with tqdm(total=total_num, desc=f"Processing {subject_name}", unit="frame", leave=False) as pbar_frames:
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                
+                
+                if total_frames - frame_count < chunk_size:
+                    break
+
+                # Crop and resize the face, and update the progress bar
+                resized_face = face_detector.detect_and_resize_face(frame, frame_count, target_size, backend, use_larger_box, larger_box_coef )
+                #print(resized_face)
+                if resized_face is not None:
+                    face_frames.append(resized_face)
+                else:
+                    face_frames.append(np.zeros((target_size[1], target_size[0], 3), dtype=np.uint8))
+                frame_count += 1
+
+                if frame_count % chunk_size == 0:
+                    face_numpy = np.array(face_frames)
                     
-                    if total_frames - frame_count < chunk_size:
-                        break
+                    face_frames_np = self.diff_normalize_data(face_numpy)
+                    
+                    # RGB npy 파일 저장
+                    rgb_output_file = os.path.join(rgb_output_dir, f"RGB_{subject_name}_input{npy_index}.npy")
+                    np.save(rgb_output_file, face_frames_np)
+                    rgb_files.append(rgb_output_file)
+                    
+                    # YUV로 변환 후 npy 파일 저장
+                    yuv_frames_np = self.rgb_to_yuv(face_frames_np)
+                    yuv_output_file = os.path.join(yuv_output_dir, f"YUV_{subject_name}_input{npy_index}.npy")
+                    np.save(yuv_output_file, yuv_frames_np)
+                    yuv_files.append(yuv_output_file)
+                    
+                    if self.config['PREPROCESS']['RANDOM_FRAME']:
+                        # 랜덤 프레임을 이미지로 저장
+                        self.save_random_frames(face_frames, image_save_dir, subject_name, npy_index)
+                    
+                    face_frames = []
+                    npy_index += 1
 
-                    resized_face = face_detector.detect_and_resize_face(frame, frame_count, target_size, backend, use_larger_box, larger_box_coef )
-                    if resized_face is not None:
-                        face_frames.append(resized_face)
-                    else:
-                        face_frames.append(np.zeros((target_size[1], target_size[0], 3), dtype=np.uint8))
-                    frame_count += 1
-
-                    if frame_count % chunk_size == 0:
-                        face_numpy = np.array(face_frames)
-                        
-                        face_frames_np = self.diff_normalize_data(face_numpy)
-                        
-                        rgb_output_file = os.path.join(rgb_output_dir, f"RGB_{subject_name}_input{npy_index}.npy")
-                        np.save(rgb_output_file, face_frames_np)
-                        rgb_files.append(rgb_output_file)
-                        
-                        yuv_frames_np = self.rgb_to_yuv(face_frames_np)
-                        yuv_output_file = os.path.join(yuv_output_dir, f"YUV_{subject_name}_input{npy_index}.npy")
-                        np.save(yuv_output_file, yuv_frames_np)
-                        yuv_files.append(yuv_output_file)
-                        
-                        if self.config['PREPROCESS']['RANDOM_FRAME']:
-                            self.save_random_frames(face_frames, image_save_dir, subject_name, npy_index)
-                        
-                        face_frames = []
-                        npy_index += 1
-
-                    pbar_frames.update(1)  
+                pbar_frames.update(1)  # Update progress bar after processing each frame
 
         cap.release()
 
@@ -240,6 +234,7 @@ class convert_vid2npy():
 
         for i in random_indices:
             frame = face_frames[i]
+            # OpenCV는 BGR 형식을 사용하므로, RGB에서 BGR로 변환하여 저장해야 함
             frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             frame_file = os.path.join(save_dir, f"{subject_name}_input{npy_index}_frame{i}.png")
             cv2.imwrite(frame_file, frame_bgr)
